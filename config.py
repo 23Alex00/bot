74c2ce310dd3c812deee01868cb07de6a8e5ff04
23Alex00 <149222798+23Alex00@users.noreply.github.com>
2023-12-12 07:08:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "6352172920:AAEXMNsNtCpu3wFtMJDYLYr3WRzM8x_GV0o"
+
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'евро': 'EUR',
+        'бинанс_коин': 'BNB',
+        'эфириум_классик': 'ETC',
+        'тон': 'TON'}
